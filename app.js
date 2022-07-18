3599ed79a14a2923ccb2f020156e077d8c2fa518
Emiliano1982 <emiliano198218@gmail.com>
2022-07-18 14:07:39 -0300

    Estructura html de Mercado Liebre

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require ("express")
+const path = require ("path")
+const app = express()
+const port = 3030
+
+app.use(express.static(path.join(__dirname,"public")))
+/*app.use(express.static(path.resolve(__dirname,"public")))*/
+app.get("/",(req,res) => res.sendFile(path.resolve(__dirname,"views","index.html")))
+
+/*app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views","index.html")))*/
+
+app.listen(port,() => console.log(`El servidor fue levantado con exito el dia Miercoles a las 15:00hs en http://localhost:${port}`))
