197cf71391249609f2741e4929ba91713b946341
Emiliano1982 <emiliano198218@gmail.com>
2022-07-18 15:38:39 -0300

    Finalizo trabajo de estructura html del sitio web

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3030
 
 app.use(express.static(path.join(__dirname,"public")))
 /*app.use(express.static(path.resolve(__dirname,"public")))*/
-app.get("/",(req,res) => res.sendFile(path.resolve(__dirname,"views","index.html")))
+app.get("/",(req,res) => res.sendFile(path.resolve(__dirname,"views","home.html")))
 
 /*app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views","index.html")))*/
 
